c004de4238d156b5b9678f3b3c2e801e82701324
Bodden007 <Denis-Bodrov@mail.ru>
2026-08-30 15:34:09 +0500

    chore: mark pinned config buffer for unmanaged allocation

--- CoreLink.Client/Buffers/PinnedConfigBuffer.cs
+++ CoreLink.Client/Buffers/PinnedConfigBuffer.cs
@@ -14,6 +14,10 @@ internal sealed class PinnedConfigBuffer : IDisposable
 
     public int Length { get; }
 
+    // FIXME: Конфигурационный FFI-буфер временно создаётся как managed byte[] и pin-ится.
+    // Заменить на unmanaged allocation (NativeMemory.Alloc/Free), чтобы память
+    // освобождалась детерминированно сразу после завершения Configure без участия GC.
+
     public PinnedConfigBuffer(byte[] buffer)
     {
         _handle = GCHandle.Alloc(
